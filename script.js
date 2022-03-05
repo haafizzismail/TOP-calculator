e1f886487f0fc862657922466a7f30d7f58f6abd
Haafizz Ismail <mdnurhaafizz@gmail.com>
2022-03-05 11:38:33 +0800

    display numbers pressed

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const display = document.querySelector('.display');
+display.textContent = '0';
+
+let numberStore = '';
+
+const numberClicks = document.querySelectorAll('.number');
+numberClicks.forEach(numberClick => {
+    numberClick.addEventListener('click', (e) => {
+        if (numberStore.charAt(0) === '0') {
+            numberStore = numberStore.substring(1);
+        }
+        numberStore += e.target.textContent;
+        display.textContent = numberStore;
+    });
+})
+
+const clear = document.querySelector('.button.clear');
+clear.addEventListener('click', clearDisplay);
+
 function add(a, b) {
     return a + b;
 }
@@ -26,4 +45,7 @@ function operate(operator, a, b) {
     }
 }
 
-
+function clearDisplay() {
+    numberStore = '';
+    display.textContent = '0';
+}
